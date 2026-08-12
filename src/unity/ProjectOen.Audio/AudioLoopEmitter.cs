64db0383711f5b299f28ffa8ef456ea9fa947654
Ternedal <a.nielbaek@gmail.com>
2026-08-12 16:03:37 +0200

    feat(audio): make loop emitters runtime-stateable

--- src/unity/ProjectOen.Audio/AudioLoopEmitter.cs
+++ src/unity/ProjectOen.Audio/AudioLoopEmitter.cs
@@ -11,13 +11,18 @@ namespace ProjectOen.Audio
     {
         [SerializeField] private AudioEventDefinition _definition;
         [SerializeField] private bool _playOnEnable = true;
+        [SerializeField, Range(0f, 1f)] private float _gain = 1f;
 
         private AudioSource _source;
+        private float _baseVolume = 1f;
+
+        public AudioEventDefinition Definition => _definition;
+        public bool IsPlaying => _source != null && _source.isPlaying;
+        public float Gain => _gain;
 
         private void Awake()
         {
-            _source = GetComponent<AudioSource>();
-            _source.playOnAwake = false;
+            EnsureSource();
         }
 
         private void OnEnable()
@@ -28,12 +33,33 @@ namespace ProjectOen.Audio
 
         private void OnDisable()
         {
+            Stop();
+        }
+
+        public void Configure(AudioEventDefinition definition, bool restartIfPlaying = true)
+        {
+            if (_definition == definition)
+                return;
+
+            var wasPlaying = IsPlaying;
+            Stop();
+            _definition = definition;
+
+            if (restartIfPlaying && wasPlaying && isActiveAndEnabled)
+                Play();
+        }
+
+        public void SetGain(float gain)
+        {
+            _gain = Mathf.Clamp01(gain);
             if (_source != null)
-                _source.Stop();
+                _source.volume = _baseVolume * _gain;
         }
 
         public bool Play()
         {
+            EnsureSource();
+
             if (_definition == null || !_definition.Loop || !_definition.TryPickClip(out var clip))
                 return false;
 
@@ -41,7 +67,8 @@ namespace ProjectOen.Audio
             _source.outputAudioMixerGroup = _definition.Output;
             _source.loop = true;
             _source.spatialBlend = _definition.SpatialBlend;
-            _source.volume = _definition.PickVolume();
+            _baseVolume = _definition.PickVolume();
+            _source.volume = _baseVolume * _gain;
             _source.pitch = _definition.PickPitch();
             _source.priority = _definition.Priority;
             _source.minDistance = _definition.MinDistance;
@@ -49,6 +76,24 @@ namespace ProjectOen.Audio
             _source.rolloffMode = _definition.RolloffMode;
             _source.Play();
             return true;
+        }
+
+        public void Stop()
+        {
+            if (_source == null)
+                return;
+
+            _source.Stop();
+            _source.clip = null;
+        }
+
+        private void EnsureSource()
+        {
+            if (_source != null)
+                return;
+
+            _source = GetComponent<AudioSource>();
+            _source.playOnAwake = false;
         }
     }
 }
